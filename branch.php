997b6b3cb518b0d856e97246a39a4613e842b4d1
lamnd27 <106128147+lamnd27@users.noreply.github.com>
2022-10-24 14:38:42 +0700

    a

--- branch.php
+++ branch.php
@@ -1,7 +1,7 @@
 
 
     <!-- Breadcrumb Section Begin -->
-    <section class="breadcrumb-section set-bg" data-setbg="ATNtoy/background.jpg">
+    <section class="breadcrumb-section set-bg" data-setbg="ATNtoy/Bagr.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
--- profile.php
+++ profile.php
@@ -44,7 +44,7 @@
         </div>
     </section>
     <!-- Hero Section End -->
-<section class="breadcrumb-section set-bg" data-setbg="ATNtoy/background.jpg">
+<section class="breadcrumb-section set-bg" data-setbg="ATNtoy/Bagr.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
