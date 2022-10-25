638f1ef78f4e00c113c3b8bf2608518b6809caa9
lamnd27 <106128147+lamnd27@users.noreply.github.com>
2022-10-25 09:58:45 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -12,7 +12,7 @@
                             <span>All departments</span>
                         </div>
                         <ul>
-                        <?php Department($conn ); ?>
+                            <?php Department($conn ); ?>
                             
                         </ul>
                     </div>
