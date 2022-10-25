6c80171b700a9dd7c20299e3a8e1bac537e69f65
lamnd27 <106128147+lamnd27@users.noreply.github.com>
2022-10-25 09:51:04 +0700

    ok

--- shop-grid.php
+++ shop-grid.php
@@ -98,72 +98,8 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="sidebar__item sidebar__item__color--option">
-                            <h4>Colors</h4>
-                            <div class="sidebar__item__color sidebar__item__color--white">
-                                <label for="white">
-                                    White
-                                    <input type="radio" id="white">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--gray">
-                                <label for="gray">
-                                    Gray
-                                    <input type="radio" id="gray">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--red">
-                                <label for="red">
-                                    Red
-                                    <input type="radio" id="red">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--black">
-                                <label for="black">
-                                    Black
-                                    <input type="radio" id="black">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--blue">
-                                <label for="blue">
-                                    Blue
-                                    <input type="radio" id="blue">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--green">
-                                <label for="green">
-                                    Green
-                                    <input type="radio" id="green">
-                                </label>
-                            </div>
-                        </div>
-                        <div class="sidebar__item">
-                            <h4>Popular</h4>
-                            <div class="sidebar__item__size">
-                                <label for="33RPM">
-                                    33RPM
-                                    <input type="radio" id="33RPM">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__size">
-                                <label for="45RPM">
-                                    45RPM
-                                    <input type="radio" id="45RPM">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__size">
-                                <label for="Signature">
-                                    Signature
-                                    <input type="radio" id="Signature">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__size">
-                                <label for="Trend">
-                                    Trend
-                                    <input type="radio" id="Trend">
-                                </label>
-                            </div>
-                        </div>
+                        
+                        
                         <div class="sidebar__item">
                             <div class="latest-product__text">
                                 <h4>Latest Products</h4>
