32eddcac6bd62eb144aad583127116aad1c6a910
lamnd27 <106128147+lamnd27@users.noreply.github.com>
2022-10-25 11:43:39 +0700

    Update Featured.php

--- Featured.php
+++ Featured.php
@@ -36,8 +36,8 @@
                 <div class="col-lg-3 col-md-4 col-sm-6 mix ">
                 
                     <div class="featured__item">
-                        <div class="featured__item__pic set-bg">
-                        <img src="ATNtoy/<?php echo $row['pro_image'] ?>" alt="" width="262px" height="270px">
+                        <div class="featured__item__pic set-bg" img src="ATNtoy/<?php echo $row['pro_image'] ?>" alt="">
+                        
                             <ul class="featured__item__pic__hover">
                     
                                 <li><a href="#"><span class="fa fa-heart"></span></a></li>
